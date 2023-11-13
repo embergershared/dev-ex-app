e16024a5a7647a6ba46805f6f5ada1833f298c4e
Emmanuel Bergerat <@>
2023-11-13 18:47:03 -0500

    added Developer exception page

--- src/ContosoUniversity.WebApplication/Program.cs
+++ src/ContosoUniversity.WebApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -34,15 +35,19 @@ if (builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"] != null)
 
 var app = builder.Build();
 
-//if (env.IsDevelopment())
-//{
-//    app.UseDeveloperExceptionPage();
-//}
-
-app.UseExceptionHandler("/Error");
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-app.UseHsts();
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    
+    // The default HSTS value is 30 days.
+    // You may want to change this for production scenarios,
+    // see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
