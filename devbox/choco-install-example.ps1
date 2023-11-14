15128fb73315c17ecbb7df618f92b7dd7bdbf433
Emmanuel Bergerat <45499891+gopher194@users.noreply.github.com>
2023-11-14 14:31:40 -0500

    updated choco packages

--- devbox/choco-install-example.ps1
+++ devbox/choco-install-example.ps1
@@ -1,4 +1,7 @@
+# Install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install software
 choco install adobereader
 choco install googlechrome
 choco install notepadplusplus
@@ -14,5 +17,19 @@ wsl --set-default-version 2
 choco install wireshark
 choco install wsl-ubuntu-2204
 choco install thunderbird
-choco install docker-desktop 
+choco install docker-desktop
 choco install postman
+
+# choco install python3
+# choco install intellijidea-community
+# choco install onedrive
+# choco install office365business
+# choco install azure-data-studio
+# choco install azure-functions-core-tools-3
+# choco install visualstudio2019enterprise
+# choco install visioviewer
+# choco install wsl-ubuntu-2004 --params "/InstallRoot:true" 
+# choco install docker-desktop
+# choco install kubernetes-helm --version=3.0.2
+# choco install nodejs-lts --version=10.16.0
+# choco install terraform
