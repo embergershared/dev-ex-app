9ff9aa815f787d074d5aaedea20b3d70ceae0907
Emmanuel <eb@mngenvmcap446692.onmicrosoft.com>
2023-11-15 14:06:19 -0500

    fixed DBInitializer

--- src/ContosoUniversity.API/Data/ContosoUniversityAPIContext.cs
+++ src/ContosoUniversity.API/Data/ContosoUniversityAPIContext.cs
@@ -10,7 +10,7 @@ namespace ContosoUniversity.API.Data
         }
 
         public DbSet<Course> Courses { get; set; }
-        public DbSet<Student> Student { get; set; }
+        public DbSet<Student> Students { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Instructor> Instructors { get; set; }
         public DbSet<StudentCourse> StudentCourse { get; set; }
@@ -18,24 +18,22 @@ namespace ContosoUniversity.API.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Course>().ToTable("tbl_Course");
-            modelBuilder.Entity<Student>().ToTable("tbl_Student");
-            modelBuilder.Entity<Department>().ToTable("tbl_Department");
-            modelBuilder.Entity<Instructor>().ToTable("tbl_Instructor");
-            modelBuilder.Entity<StudentCourse>().ToTable("tbl_StudentCourse");
+            modelBuilder.Entity<Course>().ToTable("tbl_Courses");
+            modelBuilder.Entity<Student>().ToTable("tbl_Students");
+            modelBuilder.Entity<Department>().ToTable("tbl_Departments");
+            modelBuilder.Entity<Instructor>().ToTable("tbl_Instructors");
+            modelBuilder.Entity<StudentCourse>().ToTable("tbl_StudentCourses");
             modelBuilder.Entity<StudentCourse>().HasKey(c => new { c.CourseID, c.StudentID });
 
-            //modelBuilder.Entity<StudentCourse>()
-            //    .HasOne(bc => bc.Student)
-            //    .WithMany(b => b.StudentCourse)
-            //    .HasForeignKey(bc => bc.StudentID);
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(bc => bc.Student)
+                .WithMany(b => b.StudentCourse)
+                .HasForeignKey(bc => bc.StudentID);
 
-            //modelBuilder.Entity<StudentCourse>()
-            //    .HasOne(bc => bc.Course)
-            //    .WithMany(c => c.StudentCourse)
-            //    .HasForeignKey(bc => bc.CourseID);
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(bc => bc.Course)
+                .WithMany(c => c.StudentCourse)
+                .HasForeignKey(bc => bc.CourseID);
         }
-
-        
     }
 }
