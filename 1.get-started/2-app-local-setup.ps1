b30a7f0c8a2e196a079f296143d38c708a1322cf
Emmanuel <eb@mngenvmcap446692.onmicrosoft.com>
2023-12-01 17:59:50 -0500

    local setup improved with password test

--- 1.get-started/2-app-local-setup.ps1
+++ 1.get-started/2-app-local-setup.ps1
@@ -1,3 +1,53 @@
+# Functions
+Function Test-MsSqlPasswordRequirements {
+  param(
+      [Parameter(Mandatory=$true)]
+      [System.Security.SecureString]$Password
+  )
+
+  $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password)
+  $PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+  # Check if password length is at least 8 characters
+  if ($PlainPassword.Length -lt 8) {
+      Write-Output "Password must be at least 8 characters long."
+      return $false
+  }
+
+  # Check if password contains at least one uppercase letter
+  if (-not ($PlainPassword -cmatch '[A-Z]')) {
+      Write-Output "Password must contain at least one uppercase letter."
+      return $false
+  }
+
+  # Check if password contains at least one lowercase letter
+  if (-not ($PlainPassword -cmatch '[a-z]')) {
+      Write-Output "Password must contain at least one lowercase letter."
+      return $false
+  }
+
+  # Check if password contains at least one digit
+  if (-not ($PlainPassword -cmatch '\d')) {
+      Write-Output "Password must contain at least one digit."
+      return $false
+  }
+
+  # Check if password contains at least one special character
+  if (-not ($PlainPassword -cmatch '[^a-zA-Z\d]')) {
+      Write-Output "Password must contain at least one special character."
+      return $false
+  }
+
+  # Check if the password contains any of the disallowed characters
+  if ($PlainPassword -cmatch "`0x27" -or $PlainPassword -cmatch "`0x5E" -or $PlainPassword -cmatch "`0x22") {
+    Write-Output "Password must not contain: ' or ^ or ""."
+    return $false
+  }
+
+  # If all checks passed, return true
+  return $true
+}
+
 # Set script values
 $orgName = "embergershared"
 $repoName = "dev-ex-app"
@@ -27,7 +77,22 @@ git config --global user.email "$(az ad signed-in-user show --query 'userPrincip
 
 # Create a SQL Server container for development
 docker pull mcr.microsoft.com/mssql/server:2022-latest
-$pw = Read-Host "SQL container SA account password to use (at least 8 characters long): " -AsSecureString
+# $pw = Read-Host "SQL container SA account password to use (at least 8 characters long): " -AsSecureString
+$attempts = 0
+while ($attempts -lt 3) {
+  $pw = Read-Host "SQL container SA account password to use (at least 8 characters long): " -AsSecureString
+  if (Test-MsSqlPasswordRequirements -Password $pw) {
+    break
+  }
+  $attempts++
+  Write-Output "Password does not meet the requirements. Please try again."
+}
+
+if ($attempts -eq 3) {
+  Write-Output "Maximum number of attempts (3) reached. Exiting script."
+  return
+}
+
 docker run `
   -e "ACCEPT_EULA=Y" `
   -e "MSSQL_SA_PASSWORD=$([pscredential]::new('user',$pw).GetNetworkCredential().Password)" `
