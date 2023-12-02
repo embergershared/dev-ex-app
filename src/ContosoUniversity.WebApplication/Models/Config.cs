d37f1abb2f24c3a287673cd9cacc407a572a02d6
Emmanuel <eb@mngenvmcap446692.onmicrosoft.com>
2023-12-02 00:43:51 -0500

    #6 Health check is based on checking webapi /swagger every 5 seconds

---
+++ src/ContosoUniversity.WebApplication/Models/Config.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace ContosoUniversity.WebApplication.Models
+{
+    public static class Config
+    {
+        public static Dictionary<string, string> App = new();
+    }
+}
